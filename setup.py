d4901487b2f5f091e8129ba378a1fbad6afefd20
Omar Melikechi <omar.melikechi@gmail.com>
2025-07-14 12:46:41 -0400

    0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='localgraph',
-	version='0.1.0',
+	version='0.1.1',
 	author='Omar Melikechi',
 	author_email='omar.melikechi@gmail.com',
 	description='Local graph estimation with pathwise feature selection',
