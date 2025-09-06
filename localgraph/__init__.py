bc804d10c905bcc5f318e8090dd28ec1f5ae759f
omelikechi <omar.melikechi@gmail.com>
2025-09-06 18:43:36 -0400

    0.1.2

--- localgraph/__init__.py
+++ localgraph/__init__.py
@@ -4,10 +4,11 @@ from localgraph.evaluation.eval import tp_and_fp, subgraph_within_radius
 from localgraph.pfs.helpers import lightest_paths, prune_graph
 from localgraph.pfs.main import pfs
 from localgraph.plotting.plot_graph import plot_graph
-from localgraph.utils import restrict_to_local_graph
+from localgraph.utils import max_cor_response, restrict_to_local_graph
 
 __all__ = [
 	'lightest_paths',
+	'max_cor_response',
 	'pfs',
 	'plot_graph',
 	'prune_graph',
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='localgraph',
-	version='0.1.1',
+	version='0.1.2',
 	author='Omar Melikechi',
 	author_email='omar.melikechi@gmail.com',
 	description='Local graph estimation with pathwise feature selection',
